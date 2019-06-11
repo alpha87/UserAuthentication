e0583ca71b70ff04a62e45bab39779144151430f
alpha87 <jianxun2004@gmail.com>
2019-06-11 13:35:57 +0800

    异步发送邮件

--- utils/send_email.py
+++ utils/send_email.py
@@ -16,13 +16,19 @@ EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
 EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 
 
-def send_email(receiver, link):
+def send_email(receiver, link, config="confirm"):
 
     mail_msg = """<p>仅需最后一步来激活您的帐号。点击下面的链接来验证您的邮箱地址：</p><a href="{link}">{link}</a>""".format(
         link=link)
+    mail_title = '验证您的[ 用户认证系统 ]帐号注册邮箱'
+
+    if config == "forgot":
+        mail_msg = """<p>点击下面的链接来重设您的密码：</p><a href="{link}">{link}</a>""".format(
+        link=link)
+        mail_title = '验证您的[ 用户认证系统 ]帐号重设密码邮箱'
 
     message = MIMEText(mail_msg, "html", 'utf-8')
-    message['Subject'] = '验证您的[ 用户认证系统 ]帐号注册邮箱'
+    message['Subject'] = mail_title
     message['From'] = 'jianxun<{}>'.format(EMAIL_HOST_USER)
     message['To'] = receiver
     receivers = [receiver]
